0e78261ef62c96e9acd79f06e05da16ff3c890d7
MikeCAT <mikecat@users.noreply.github.com>
2025-04-16 19:30:18 +0900

    update license year

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyProduct("いい感じにブレーキをかけたい")]
 [assembly: AssemblyTitle("何って…止まるまでの距離に応じてブレーキを調整しただけだが？")]
 [assembly: AssemblyCompany("みけCAT")]
-[assembly: AssemblyCopyright("Copyright (c) 2024 みけCAT")]
+[assembly: AssemblyCopyright("Copyright (c) 2024-2025 みけCAT")]
--- ReadMe.txt
+++ ReadMe.txt
@@ -154,7 +154,7 @@ TRAIN CREW 用の運転支援ツールです。
 「いい感じにブレーキをかけたい」は、MITライセンスです。
 
 ```
-Copyright (c) 2024 みけCAT
+Copyright (c) 2024-2025 みけCAT
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
