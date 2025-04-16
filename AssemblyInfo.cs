9753e50643d3a4eb51fb9acc4acccea9fd9d3079
MikeCAT <mikecat@users.noreply.github.com>
2025-04-16 22:44:11 +0900

    increment version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyProduct("いい感じにブレーキをかけたい")]
 [assembly: AssemblyTitle("何って…止まるまでの距離に応じてブレーキを調整しただけだが？")]
 [assembly: AssemblyCompany("みけCAT")]
